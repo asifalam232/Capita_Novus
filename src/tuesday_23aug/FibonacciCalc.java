c63fea9eadc645fcbf11c374a22331dc180fef7c
Asif Alam <alamtheboss232@gmail.com>
2022-08-24 10:33:58 +0100

    Add Class FibonacciCalc that takes input from the user and prints out the first Nth elements of the Fibonacci sequence.

---
+++ src/tuesday_23aug/FibonacciCalc.java
@@ -0,0 +1,29 @@
+package tuesday_23aug;
+
+import java.util.Scanner;
+
+public class FibonacciCalc {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many Fibonacci numbers you want in Integer form: ");
+        int n = scanner.nextInt();
+        System.out.println("The first " + n + " fibonacci numbers are: ");
+        Fibonacci(n);
+    }
+
+    public static void Fibonacci (int n) {
+        long num1 = 0;
+        long num2 = 1;
+        int counter = 0;
+//        long sum = 0;
+
+        while (counter < n) {
+            System.out.print(num1 + ", ");
+            long sum = num2 + num1;
+            num1 = num2;
+            num2 = sum;
+            counter++;
+        }
+    }
+}
