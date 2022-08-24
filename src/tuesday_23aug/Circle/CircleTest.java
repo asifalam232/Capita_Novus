3d479b2dacfce5d3c24d5b28fb17bb5b6904cfef
Asif Alam <alamtheboss232@gmail.com>
2022-08-24 12:31:23 +0100

    Add Circle and CircleTest classes

---
+++ src/tuesday_23aug/Circle/CircleTest.java
@@ -0,0 +1,10 @@
+package tuesday_23aug.Circle;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle1 =new Circle(3);
+        Circle circle2 = new Circle();
+        System.out.println(circle1);
+        System.out.println(circle2);
+    }
+}
